bcb6d1bc273a05685a3a20c3504e3c1048a88ebe
Jonathan Bass <jonathan.bass@parexel.com>
2018-04-19 10:51:52 +0100

    Added SQL connection information

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,9 +1,15 @@
-import {Sequelize} from 'sequelize-typescript';
+import { Sequelize } from 'sequelize-typescript';
 
 export const sequelize = new Sequelize({
-  dialect: 'mysql',
-  database: 'movies',
-  username: 'root',
-  password: '',
+  dialect: 'mssql',
+  username: 'sa',
+  password: 'xxxx',
+  dialectModulePath: 'sequelize-msnodesqlv8',
+  dialectOptions: {
+    instanceName: '(localdb)\MSSQLLocalDB',
+    trustedConnection: true
+  },
+  host: 'localhost',
+  database: 'test-db',
   modelPaths: [__dirname + '/models']
 });
